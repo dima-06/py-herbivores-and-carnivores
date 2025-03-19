93427cec552eb62d473a657d255b10b8bbc9f51b
Dmytro Hrabovskyi <dima.grabovskiy2000@gmail.com>
2025-03-19 22:55:28 +0200

    solutio 1.0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+    @classmethod
+    def get_alive(cls) -> list:
+        return [repr(animal) for animal in cls.alive]
+
+
+class Herbivore(Animal):
+    def hide(self) -> bool:
+        self.hidden = not self.hidden
+        return self.hidden
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(second_animal: Herbivore) -> int:
+        if (isinstance(second_animal, Herbivore)
+                and not second_animal.hidden):
+            second_animal.health -= 50
+            if second_animal.health <= 0:
+                Animal.alive.remove(second_animal)
+        second_animal.health = second_animal.health
+        return second_animal.health
